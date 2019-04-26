3714a308fb8c85ecd4d74db9b35f9153605485eb
Loris Leitner <lorisleitner@live.com>
2019-04-26 15:44:52 +0200

    Add content

---
+++ js/ismycommitmessageuseful.js
@@ -0,0 +1,105 @@
+const baseApiUrl = "https://ismycommitmessageuseful.herokuapp.com/api/";
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var rateCommits;
+var currentRateCommit;
+
+$(document).ready(function () {
+    loadRateCommits();
+});
+
+$("#checkMessageButton").on("click", async function () {
+    console.log($("#messageInput").val());
+    $(this).prop("disabled", true);
+    $(this).html("<span class=\"spinner-border spinner-border-sm\" aria-hidden=\"true\"></span>Loading...");
+
+    // TODO: GET cannot have a body, change API method to accept message parameters from URL
+    fetch("https://ismycommitmessageuseful.herokuapp.com/api/predict", {
+        method: "GET",
+        cache: "no-cache",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            message: $("#messageInput").text()
+        }
+    })
+
+    $(this).prop("disabled", false);
+    $(this).html("Check");
+});
+
+$("#rateCommitUsefulButton").on("click", function () {
+    fetch(baseApiUrl + "commits/" + currentRateCommit.id + "/useful", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache"
+    });
+
+    nextRateCommit();
+});
+
+$("#rateCommitNotUsefulButton").on("click", function () {
+    fetch(baseApiUrl + "commits/" + currentRateCommit.id + "/notuseful", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache"
+    });
+
+    nextRateCommit();
+});
+
+$("#rateCommitDontKnowButton").on("click", function () {
+    fetch(baseApiUrl + "commits/" + currentRateCommit.id + "/dontknow", {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache"
+    });
+
+    nextRateCommit();
+});
+
+function loadRateCommits() {
+    $("#rateCommitMessage").text("Loading...");
+    $("#rateCommitUsefulButton").prop("disabled", true);
+    $("#rateCommitNotUsefulButton").prop("disabled", true);
+    $("#rateCommitDontKnowButton").prop("disabled", true);
+
+    fetch("https://ismycommitmessageuseful.herokuapp.com/api/commits", {
+        method: "GET",
+        cache: "no-cache",
+        mode: "cors"
+    })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then(function (commits) {
+
+            console.log("rate commits loaded");
+            rateCommits = commits;
+
+            $("#rateCommitUsefulButton").prop("disabled", false);
+            $("#rateCommitNotUsefulButton").prop("disabled", false);
+            $("#rateCommitDontKnowButton").prop("disabled", false);
+
+            nextRateCommit();
+        });
+}
+
+function nextRateCommit() {
+    var commit = rateCommits.pop();
+
+    if (commit !== undefined) {
+        currentRateCommit = commit;
+
+        $("#rateCommitMessage").text(currentRateCommit.message);
+    }
+    else {
+        loadRateCommits();
+    }
+}
